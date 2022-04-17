b341e1129471ddaeda34c6316bf6a6303ff4fef4
AbdillahRomadhon <abdillahromadhon512@gmail.com>
2022-04-17 20:57:48 +0700

    daftar notulensi beberapa data statis

--- resources/views/abdill.blade.php
+++ resources/views/abdill.blade.php
@@ -36,10 +36,6 @@
             <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
             <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i class="fas fa-search"></i></a></li>
           </ul>
-          <div class="search-element">
-            <input class="form-control" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn" type="submit"><i class="fas fa-search"></i></button>
-          </div>
         </form>
         <ul class="navbar-nav navbar-right">
           <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown" class="nav-link notification-toggle nav-link-lg beep"><i class="far fa-bell"></i></a>
@@ -154,21 +150,127 @@
             </div>
           </div>
 
+          <form class="form-inline mr-auto">
+            <div class="search-element">
+              <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn" type="submit"><i class="fas fa-search"></i></button>
+              <br>
+            </div>
+          </form>
+
+          <br>
+          {{-- card --}}  
           <div class="section-body">
-            <h2 class="section-title">This is Example Page</h2>
-            <p class="section-lead">This page is just an example for you to create your own page.</p>
             <div class="card">
-              <div class="card-header">
+              {{-- <div class="card-header">
                 <h4>Example Card</h4>
+              </div> --}}
+              
+              <div class="table-responsive">
+                <div id="table-1_wrapper" class="dataTables_wrapper container-fluid dt-bootstrap4 no-footer"><div class="row"><div class="col-sm-12"><table class="table table-striped dataTable no-footer" id="table-1" role="grid" aria-describedby="table-1_info">
+                  <thead>                                 
+                    <tr role="row">
+                      <th class="text-center sorting_asc" tabindex="0" aria-controls="table-1" rowspan="1" colspan="1" aria-sort="ascending" aria-label="
+                        #
+                      : activate to sort column descending" style="width: 18.6771px;">
+                        No.
+                      </th>
+                      <th class="sorting" tabindex="0" aria-controls="table-1" rowspan="1" colspan="1" aria-label="Task Name: activate to sort column ascending" style="width: 132.542px;">
+                        Nomor Undangan
+                      </th>
+                      <th class="sorting_disabled" rowspan="1" colspan="1" aria-label="Progress" style="width: 132.542px;">
+                        Agenda
+                      </th>
+                      <th class="sorting_disabled" rowspan="1" colspan="1" aria-label="Members" style="width: 200px;">
+                        Pimpinan
+                      </th>
+                      <th class="sorting" tabindex="0" aria-controls="table-1" rowspan="1" colspan="1" aria-label="Due Date: activate to sort column ascending" style="width: 150px;">
+                        Hari, Tanggal
+                      </th>
+                      <th class="sorting" tabindex="0" aria-controls="table-1" rowspan="1" colspan="1" aria-label="Action: activate to sort column ascending" style="width: 63.1875px;">
+                        Action
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>                                 
+                    
+                    
+                    
+                    
+                  <tr role="row" class="odd">
+                      <td class="sorting_1">
+                        1
+                      </td>
+                      <td>6970/PL14/TU/2021</td>
+                      <td class="align-middle">
+                        Rapat Perubahan Kurikulum
+                      </td>
+                      <td>
+                        Ali Ridho Barakbah, S. Kom., Ph.D.
+                      </td>
+                      <td>Senin, 30 November 2021</td>
+                      <td>
+                        <i class="fa-solid fa-edit"></i>
+                        <i class="fa-solid fa-delete"></i>
+                      </td>
+                    </tr><tr role="row" class="even">
+                      <td class="sorting_1">
+                        2
+                      </td>
+                      <td>Redesign homepage</td>
+                      <td class="align-middle">
+                        <div class="progress" data-height="4" data-toggle="tooltip" title="" data-original-title="0%" style="height: 4px;">
+                          <div class="progress-bar" data-width="0" style="width: 0px;"></div>
+                        </div>
+                      </td>
+                      <td>
+                        <img alt="image" src="assets/img/avatar/avatar-1.png" class="rounded-circle" width="35" data-toggle="tooltip" title="" data-original-title="Nur Alpiana">
+                        <img alt="image" src="assets/img/avatar/avatar-3.png" class="rounded-circle" width="35" data-toggle="tooltip" title="" data-original-title="Hariono Yusup">
+                        <img alt="image" src="assets/img/avatar/avatar-4.png" class="rounded-circle" width="35" data-toggle="tooltip" title="" data-original-title="Bagus Dwi Cahya">
+                      </td>
+                      <td>2018-04-10</td>
+                      <td><div class="badge badge-info">Todo</div></td>
+                      <td><a href="#" class="btn btn-secondary">Detail</a></td>
+                    </tr><tr role="row" class="odd">
+                      <td class="sorting_1">
+                        3
+                      </td>
+                      <td>Backup database</td>
+                      <td class="align-middle">
+                        <div class="progress" data-height="4" data-toggle="tooltip" title="" data-original-title="70%" style="height: 4px;">
+                          <div class="progress-bar bg-warning" data-width="70%" style="width: 70%;"></div>
+                        </div>
+                      </td>
+                      <td>
+                        <img alt="image" src="assets/img/avatar/avatar-1.png" class="rounded-circle" width="35" data-toggle="tooltip" title="" data-original-title="Rizal Fakhri">
+                        <img alt="image" src="assets/img/avatar/avatar-2.png" class="rounded-circle" width="35" data-toggle="tooltip" title="" data-original-title="Hasan Basri">
+                      </td>
+                      <td>2018-01-29</td>
+                      <td><div class="badge badge-warning">In Progress</div></td>
+                      <td><a href="#" class="btn btn-secondary">Detail</a></td>
+                    </tr><tr role="row" class="even">
+                      <td class="sorting_1">
+                        4
+                      </td>
+                      <td>Input data</td>
+                      <td class="align-middle">
+                        <div class="progress" data-height="4" data-toggle="tooltip" title="" data-original-title="100%" style="height: 4px;">
+                          <div class="progress-bar bg-success" data-width="100%" style="width: 100%;"></div>
+                        </div>
+                      </td>
+                      <td>
+                        <img alt="image" src="assets/img/avatar/avatar-2.png" class="rounded-circle" width="35" data-toggle="tooltip" title="" data-original-title="Rizal Fakhri">
+                        <img alt="image" src="assets/img/avatar/avatar-5.png" class="rounded-circle" width="35" data-toggle="tooltip" title="" data-original-title="Isnap Kiswandi">
+                        <img alt="image" src="assets/img/avatar/avatar-4.png" class="rounded-circle" width="35" data-toggle="tooltip" title="" data-original-title="Yudi Nawawi">
+                        <img alt="image" src="assets/img/avatar/avatar-1.png" class="rounded-circle" width="35" data-toggle="tooltip" title="" data-original-title="Khaerul Anwar">
+                      </td>
+                      <td>2018-01-16</td>
+                      <td><div class="badge badge-success">Completed</div></td>
+                      <td><a href="#" class="btn btn-secondary">Detail</a></td>
+                    </tr></tbody>
+                </table></div></div><div class="row"><div class="col-sm-12 col-md-5"><div class="dataTables_info" id="table-1_info" role="status" aria-live="polite">Showing 1 to 4 of 4 entries</div></div><div class="col-sm-12 col-md-7"><div class="dataTables_paginate paging_simple_numbers" id="table-1_paginate"><ul class="pagination"><li class="paginate_button page-item previous disabled" id="table-1_previous"><a href="#" aria-controls="table-1" data-dt-idx="0" tabindex="0" class="page-link">Previous</a></li><li class="paginate_button page-item active"><a href="#" aria-controls="table-1" data-dt-idx="1" tabindex="0" class="page-link">1</a></li><li class="paginate_button page-item next disabled" id="table-1_next"><a href="#" aria-controls="table-1" data-dt-idx="2" tabindex="0" class="page-link">Next</a></li></ul></div></div></div></div>
               </div>
-              <div class="card-body">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-              </div>
+
               <div class="card-footer bg-whitesmoke">
                 This is card footer
               </div>
@@ -178,10 +280,10 @@
       </div>
       <footer class="main-footer">
         <div class="footer-left">
-          Copyright © 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
+          Copyright &copy; 2022 Kelompok A2. All rights reserved.
         </div>
         <div class="footer-right">
-          v2.0.0
+          2.3.0
         </div>
       </footer>
     </div>

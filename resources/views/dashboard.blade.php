25b3123afb64b75c937f38ff132e482d6c44ccc2
AbdillahRomadhon <abdillahromadhon512@gmail.com>
2022-05-02 21:51:37 +0700

    dasboard jadi satu dosen dan sekretaris

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -16,9 +16,11 @@
    	<div class="section-header bg-white">
         <h1>Daftar Notulensi</h1>
 		<div class="section-header-breadcrumb transparent">
+			{{-- hanya untuk dipage sekretaris --}}
 			<a href="documentation.html" class="btn btn-primary btn-lg btn-icon-split btn-block">
 				<div><i class="fas fa-plus"></i>Tambah Notulensi</div>
 			</a>
+			{{-- untuk dosen dan pegawai tidak ada button ini --}}
 		</div>
     </div>
 	{{-- End Header Section --}}
@@ -62,6 +64,7 @@
 									<td class="align-middle">Rapat Perubahan Kurikulum</td>
 									<td class="align-middle">Ali Ridho Barakbah, S. Kom., Ph.D.</td>
 									<td class="align-middle">Senin, 30 November 2021</td>
+									{{-- action untuk sekretaris --}}
 									<td class="d-flex justify-content-center">
 										<div class="row w-100">
 											<div class="col-12 d-flex justify-content-between">
@@ -80,9 +83,14 @@
 									<td class="align-middle">Rapat Perubahan Kurikulum</td>
 									<td>Ali Ridho Barakbah, S. Kom., Ph.D.</td>
 									<td>Senin, 30 November 2021</td>
-									<td>
-										<i class="fa-solid fa-edit"></i>
-										<i class="fa-solid fa-delete"></i>
+									{{-- action Untuk Pegawai dosen --}}
+									<td class="d-flex justify-content-center">
+										<div class="row w-100">
+											<div class="col-12 d-flex justify-content-between">
+												<a class="btn btn-primary btn-sm text-white w-50 mr-1" title="detail" data-toggle="modal" data-target="#exampleModal"><i class="fa-solid fa-circle-info"></i></a>
+												<a class="btn btn-info btn-sm text-white w-50 ml-1" title="Delete"><i class="fa-solid fa-file-arrow-down"></i></a>
+											</div>
+										</div>
 									</td>
 								</tr>
 								<tr role="row" class="odd">
@@ -91,9 +99,16 @@
 									<td class="align-middle">Rapat Perubahan Kurikulum</td>
 									<td>Ali Ridho Barakbah, S. Kom., Ph.D.</td>
 									<td>Senin, 30 November 2021</td>
-									<td>
-										<i class="fa-solid fa-edit"></i>
-										<i class="fa-solid fa-delete"></i>
+									<td class="d-flex justify-content-center">
+										<div class="row w-100">
+											<div class="col-12 d-flex justify-content-between">
+												<a class="btn btn-primary btn-sm text-white w-50 mr-1" title="Edit"><i class="fas fa-edit"></i></a>
+												<a class="btn btn-danger btn-sm text-white w-50 ml-1" title="Delete"><i class="fas fa-trash"></i></a>
+											</div>
+											<div class="col-12 d-flex justify-content-center mt-2">
+												<a class="btn btn-info w-100 btn-sm text-white" title="Detail" data-toggle="modal" data-target="#exampleModal"><i class="fas fa-"></i>&nbsp;Detail</a>
+											</div>
+										</div>
 									</td>
 								</tr>
 								<tr role="row" class="odd">
@@ -102,9 +117,16 @@
 									<td class="align-middle">Rapat Perubahan Kurikulum</td>
 									<td>Ali Ridho Barakbah, S. Kom., Ph.D.</td>
 									<td>Senin, 30 November 2021</td>
-									<td>
-										<i class="fa-solid fa-edit"></i>
-										<i class="fa-solid fa-delete"></i>
+									<td class="d-flex justify-content-center">
+										<div class="row w-100">
+											<div class="col-12 d-flex justify-content-between">
+												<a class="btn btn-primary btn-sm text-white w-50 mr-1" title="Edit"><i class="fas fa-edit"></i></a>
+												<a class="btn btn-danger btn-sm text-white w-50 ml-1" title="Delete"><i class="fas fa-trash"></i></a>
+											</div>
+											<div class="col-12 d-flex justify-content-center mt-2">
+												<a class="btn btn-info w-100 btn-sm text-white" title="Detail" data-toggle="modal" data-target="#exampleModal"><i class="fas fa-"></i>&nbsp;Detail</a>
+											</div>
+										</div>
 									</td>
 								</tr>
 								{{-- <tr role="row" class="even">
@@ -153,11 +175,155 @@
 					</button>
 				</div>
 				<div class="modal-body">
-					...
+					<table>
+						<tr>
+						  <td>
+							Nomor Undangan
+						  </td>
+						  <td>
+							: 
+						  </td>
+						  <td>
+							6970/PL14/TU/2021 
+						  </td>
+						</tr>
+						<tr>
+						  <td>
+							Agenda
+						  </td>
+						  <td>
+							: 
+						  </td>
+						  <td>
+							Rapat Perubahan Kurikulum
+						  </td>
+						</tr>
+						<tr>
+						  <td>
+							Pimpinan Rapat
+						  </td>
+						  <td>
+							: 
+						  </td>
+						  <td>
+							Ali Ridho Barakbah, S. Kom., Ph.D. 
+						  </td>
+						</tr>
+						<tr>
+						  <td>
+							Tanggal Rapat
+						  </td>
+						  <td>
+							: 
+						  </td>
+						  <td>
+							Senin, 30 November 2021
+						  </td>
+						</tr>
+						<tr>
+						  <td>
+							Waktu Rapat
+						  </td>
+						  <td>
+							: 
+						  </td>
+						  <td>
+							13.00 - 15.00 WIB
+						  </td>
+						</tr>
+						<tr>
+						  <td>
+							Ruang/Lokasi
+						  </td>
+						  <td>
+							: 
+						  </td>
+						  <td>
+							Pasca Sarjana
+						  </td>
+						</tr>
+						<tr>
+						  <td>
+							Jenis Agenda
+						  </td>
+						  <td>
+							: 
+						  </td>
+						  <td>
+							Offline
+						  </td>
+						</tr>
+						<tr>
+						  <td>
+							Notulen Rapat
+						  </td>
+						  <td>
+							: 
+						  </td>
+						  <td>
+							Shinta
+						  </td>
+						</tr>
+						<tr>
+						  <td>
+							Peserta Rapat
+						  </td>
+						  <td>
+							: 
+						  </td>
+						  <td>
+							...
+						  </td>
+						</tr>
+						<tr>
+						  <td>
+							Total Peserta Rapat
+						  </td>
+						  <td>
+							: 
+						  </td>
+						  <td>
+							10
+						  </td>
+						</tr>
+						<tr>
+						  <td>
+							Detail Rapat
+						  </td>
+						  <td>
+							: 
+						  </td>
+						  <td>
+							...
+						  </td>
+						</tr>
+						<tr>
+						  <td>
+							File Dokumentasi
+						  </td>
+						  <td>
+							: 
+						  </td>
+						  <td>
+							...
+						  </td>
+						</tr>
+						<tr>
+						  <td>
+							Notulensi Rapat
+						  </td>
+						  <td>
+							: 
+						  </td>
+						  <td>
+							...
+						  </td>
+						</tr>
+					  </table>
 				</div>
 				<div class="modal-footer">
-					<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-					<button type="button" class="btn btn-primary">Save changes</button>
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Keluar</button>
+					<button type="button" class="btn btn-primary">Lanjut</button>
 				</div>
 			</div>
 		</div>

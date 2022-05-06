9d47c0d338d27892a5006afafc656c9269c181ae
AbdillahRomadhon <abdillahromadhon512@gmail.com>
2022-05-06 17:58:19 +0700

    data tidak hilang ketika terjadi error saat input

--- resources/views/notulensi/input.blade.php
+++ resources/views/notulensi/input.blade.php
@@ -33,11 +33,11 @@
 						<div class="row">
 							<div class="col-md">
 								<label for="nomer" class="col-form-label">Nomer Undangan <b class="text-danger">*</b></label>
-								<input type="text" class="form-control" id="nomer" name="no_undangan">
+								<input type="text" class="form-control" id="nomer" name="no_undangan" value="{{ old('no_undangan') }}">
 							</div>
 							<div class="col-md">
 								<label for="tanggal" class="col-form-label">Tanggal Rapat <b class="text-danger">*</b></label>
-								<input type="date" class="form-control" id="tanggal" name="tgl">
+								<input type="date" class="form-control" id="tanggal" name="tgl" value="{{ old('tgl') }}">
 							</div>
 						</div>
 						<div class="row">
@@ -52,7 +52,7 @@
 							</div>
 							<div class="col-md">
 								<label for="waktu" class="col-form-label">Waktu Rapat <b class="text-danger">*</b></label>
-								<input type="time" class="form-control" id="waktu" name="waktu">
+								<input type="time" class="form-control" id="waktu" name="waktu" value="{{ old('waktu') }}">
 							</div>
 						</div>
 						<div class="row">
@@ -67,7 +67,7 @@
 							</div>
 							<div class="col-md">
 								<label for="detail" class="col-form-label">Detail Rapat <b class="text-danger">*</b></label>
-								<textarea class="form-control" id="detail" name="detail_rapat"></textarea>
+								<textarea class="form-control" id="detail" name="detail_rapat">{{ old('detail_rapat') }}</textarea>
 							</div>
 						</div>
 						<div class="row">
@@ -82,20 +82,20 @@
 							</div>
 							<div class="col-md">
 								<label for="notulensi" class="col-form-label">Agenda <b class="text-danger">*</b></label>
-								<input type="text" class="form-control" id="notulensi" name="agenda">
+								<input type="text" class="form-control" id="notulensi" name="agenda" value="{{ old('agenda') }}">
 							</div>
 						</div>
 						<div class="row">
 							<div class="col-md">
 								<label for="agenda" class="col-form-label">Jumlah Agenda <b class="text-danger">*</b></label>
-								<input type="number" class="form-control" id="agenda" name="jml_agenda">
+								<input type="number" class="form-control" id="agenda" name="jml_agenda" value="{{ old('jml_agenda') }}">
 							</div>
 							<div class="col-md">
 								<label for="total-peserta" class="col-form-label">Tamu Rapat <b class="text-danger">*</b></label>
 								<select class="form-control select2" id="total-peserta" name="tamu[]" multiple="multiple">>
 									{{-- <option selected>Cari</option> --}}
 									@foreach($pegawai as $a)
-									<option value="{{$a->id}}">{{$a->name}}</option>
+										<option value="{{$a->id}}">{{$a->name}}</option>
 									@endforeach
 								</select>
 							</div>
@@ -110,16 +110,15 @@
 							
 							<div class="col-md">
 								<label for="peserta" class="col-form-label">File Peserta Rapat (.csv)</label><br>
-								
 								<input type="file" class="form-control" id="peserta" name="file_peserta_rapat">
 							</div>
 							<div class="col-md">
 								<label for="detail" class="col-form-label">Email Peserta Rapat</label>
-								<input type="text" class="form-control" id="detail" name="peserta_rapat">
+								<input type="text" class="form-control" id="detail" name="peserta_rapat" value="{{ old('peserta_rapat') }}">
 							</div>
 							<div class="col-md">
 								<label for="detail" class="col-form-label">Jumlah Peserta Rapat</label>
-								<input type="number" class="form-control" id="detail" name="jml_peserta_rapat">
+								<input type="number" class="form-control" id="detail" name="jml_peserta_rapat" value="{{ old('jml_peserta_rapat') }}">
 							</div>
 						</div>
 						<br>

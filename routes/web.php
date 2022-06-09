47be5c01519ac453d3c87ceaf356010c1016771b
mohammad subkhan <subkhanpinter22@gmail.com>
2022-06-09 13:24:00 +0700

    hps landing

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-	// return redirect()->route('dashboard.notulensi');
-	return view('landing');
+	return redirect()->route('dashboard.notulensi');
+	// return view('landing');
 })->name('/');
 
 

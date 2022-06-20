0e67f35a4321bb0c6ef2bbfdb34b31732e0d97aa
Waridun Nafis <waridunnafis@gmail.com>
2022-06-20 18:51:16 +0700

    landing page logo update

--- resources/views/landing/layouts/app.blade.php
+++ resources/views/landing/layouts/app.blade.php
@@ -9,6 +9,7 @@
     <meta name="author" content="">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:100,200,300,400,500,600,700,800,900" rel="stylesheet">
 
+    <link rel="icon" type="image/x-icon" href="{{ asset('landing/assets/images/noraid-logo-sm.png') }}">
     <title>Nora id - Platform Notulensi Rapat Indonesia</title>
 
     <!-- Bootstrap core CSS -->
